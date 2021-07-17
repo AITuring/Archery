df2c19b2a6cb1b9a11154d87a45e23292d64f292
lishengyu <lishengyu@baidu.com>
2021-07-17 15:09:52 +0800

    用tailwindcss试试构建色彩体系

--- src/Color.tsx
+++ src/Color.tsx
@@ -3,9 +3,32 @@ import './color.css';
 
 const Color = () => {
     return (
-        <div className="board">
-          <div className="square blueColor">#44cef6</div>
-          <div className="square blueColor">#44cef6</div>
+        <div className="boar">
+          <div className="square title bg-gray-200">绿松石turquoise</div>
+          <div className="square turquoiseColor">#41d3bd</div>
+          <div className="square turquoiseColor1">#5AD8C5</div>
+          <div className="square turquoiseColor2">#65DBC8</div>
+          <div className="square turquoiseColor3">#6FDDCB</div>
+          <div className="square turquoiseColor4">#81E2D0</div>
+          <div className="square turquoiseColor5">#93E6D5</div>
+          <div className="square title">MSG green</div>
+          <div className="square msuColor">#08211E</div>
+          <div className="square msuColor1">#0E332E</div>
+          <div className="square msuColor2">#103631</div>
+          <div className="square msuColor3">#123833</div>
+          <div className="square msuColor4">#163D37</div>
+          <div className="square msuColor5">#1E4740</div>
+          <div className="square msuColor6">#2E5A52</div>
+          <div className="square msuColor7">#4C7269</div>
+          <div className="square title">翠绿emerald</div>
+          <div className="square emerald">#5AC167</div>
+          <div className="square emerald1">#63D471</div>
+          <div className="square emerald2">#6DD77A</div>
+          <div className="square emerald3">#72D97F</div>
+          <div className="square emerald4">#77DA83</div>
+          <div className="square emerald5">#8ADF95</div>
+          <div className="square emerald6">#9EE5A7</div>
+          <div className="square emerald7">#B1EAB8</div>
         </div>
     )
 }
