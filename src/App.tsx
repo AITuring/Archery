13728487cf22e6c04190b74a5d390c68a8d08c7b
AITuring <shspku365@gmail.com>
2021-08-03 01:53:58 +0800

    重温了一下

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Button from './components/Button';
 import './App.less';
+import { ButtonSize, ButtonType } from './components/Button/type';
 
 function App() {
 
   return (
     <div className="App">
-      <Button>Click me!</Button>
+      <Button
+        btnType={ButtonType.Primary}
+        size={ButtonSize.Large}
+      >Click me!</Button>
+      <Button
+        btnType={ButtonType.Primary}
+        size={ButtonSize.Large}
+        disabled
+      >Click me!</Button>
+      <Button
+        btnType={ButtonType.Link}
+        size={ButtonSize.Large}
+        href="http://www.google.com"
+      >Google</Button>
     </div>
   );
 }
